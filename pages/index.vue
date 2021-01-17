e01ac9355fc32ee2da9d49bbfec4a55e011e6a55
jmanuellh <manuellh2008@gmail.com>
2021-01-17 05:13:00 -0600

    agregado formulario basico de agregar candidato sin funcionalidad

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,31 @@
-<template>
-  <v-row justify="center" align="center">
-    <v-col cols="12" sm="8" md="6">
-      <div class="text-center">
-        <logo />
-        <vuetify-logo />
-      </div>
-      <v-card>
-        <v-card-title class="headline">
-          Welcome to the Vuetify + Nuxt.js template
-        </v-card-title>
-        <v-card-text>
-          <p>Vuetify is a progressive Material Design component framework for Vue.js. It was designed to empower developers to create amazing applications.</p>
-          <p>
-            For more information on Vuetify, check out the <a
-              href="https://vuetifyjs.com"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              documentation
-            </a>.
-          </p>
-          <p>
-            If you have questions, please join the official <a
-              href="https://chat.vuetifyjs.com/"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="chat"
-            >
-              discord
-            </a>.
-          </p>
-          <p>
-            Find a bug? Report it on the github <a
-              href="https://github.com/vuetifyjs/vuetify/issues"
-              target="_blank"
-              rel="noopener noreferrer"
-              title="contribute"
-            >
-              issue board
-            </a>.
-          </p>
-          <p>Thank you for developing with Vuetify and I look forward to bringing more exciting features in the future.</p>
-          <div class="text-xs-right">
-            <em><small>&mdash; John Leider</small></em>
-          </div>
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Nuxt GitHub
-          </a>
-        </v-card-text>
-        <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-col>
-  </v-row>
+<template lang="pug">
+div
+  h1 Candidatos
+  v-data-table(
+    :headers="headers"
+    :items="candidatos"
+  )
+    template( v-slot:top )
+      v-toolbar( flat )
+        v-toolbar-title Candidatos
+        v-spacer
+        v-dialog( v-model="dialogNuevo" max-width="500px" )
+          template( v-slot:activator="{on, attrs}" )
+            v-btn(@click="agregarCandidato()" color="primary" v-bind="attrs" v-on="on") Nuevo
+          v-card
+            v-card-text
+              v-container
+                v-row
+                  v-col(class="d-flex flex-column")
+                    v-text-field(v-model="nuevoCandidato.nombre" label ="Nombre")
+                    v-text-field(v-model.number="nuevoCandidato.telefono" label="Número" type="number" )
+                    v-text-field(v-model="nuevoCandidato.comentarios" label="Comentarios")
+            v-card-actions
+              v-spacer
+              v-btn(@click="() => dialogNuevo = false" color="primary") Cerrar
+              v-btn(@click="() => dialogNuevo = false" color="primary") Agregar
+    template(v-slot:item.acciones="{ item }")
+      v-btn(@click="eliminar" color="primary")
 </template>
 
 <script>
@@ -84,6 +36,32 @@ export default {
   components: {
     Logo,
     VuetifyLogo
+  },
+  data() {
+    return {
+      nuevoCandidato: {},
+      headers: [
+        {
+          text: 'Nombre',
+          value: 'nombre'
+        },
+        {
+          text: 'Teléfono',
+          value: 'telefono'
+        },
+        {
+          text: 'Comentarios',
+          value: 'comentarios'
+        }
+      ],
+      candidatos: [],
+      dialogNuevo: false
+    }
+  },
+  methods: {
+    agregarCandidato() {
+
+    }
   }
 }
 </script>
